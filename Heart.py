df139c901a8cfe62643a903e4440e6cdcf28039f
poonamchaudharyuma <CPunam791@gmail.com>
2019-02-09 13:19:24 +0530

    by poonam

---
+++ Heart.py
@@ -0,0 +1,28 @@
+from turtle import *
+t=Turtle()
+t.pensize(5)
+t.color("red","pink")
+t.begin_fill()
+t.left(140)
+t.forward(180 )
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180 )
+t.up()
+t.goto(-40,150)
+t.write("")
+t.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+done()
