5fd91cab7f65a7fefeffc8e26d9f399789747287
Ashwani <aswanibtech@gmail.com>
2019-02-25 13:52:10 +0530

    Data base(CRUD) by ashwani

---
+++ dbconnect.py
@@ -0,0 +1,19 @@
+import pymysql
+conn=pymysql.connect(host="localhost",user="root",db="bit")
+mycursor=conn.cursor()
+'''mycursor.execute(""" create table detail
+                     (id int primary key,
+                     name varchar(20)
+                     )""")'''
+#mycursor.execute(" insert into detail(id,name)values(2,'Ram');")
+#print("Data inserted successfully")
+#mycursor.execute(" update detail set name='Shyam' where id=2;")
+#print("Data updated successfully")
+mycursor.execute(" delete from detail where id=2;")
+print("Data deleted successfully")
+
+conn.commit()
+conn.close()
+
+
+
